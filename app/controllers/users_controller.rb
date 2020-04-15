9acff0a793dccdf95e78969c9bf953e505290403
mimisenn <53231640+mimisenn@users.noreply.github.com>
2020-04-15 18:23:33 +0900

    タイムゾーン　修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,15 @@ class UsersController < ApplicationController
   impressionist :actions=> [:show, :index]
 
   def index
+
+    start_date = Time.current.beginning_of_day
+    end_date = Time.current.end_of_day
     @users = User.all
     @users = User.search(params[:search])
     @user = User.all.sum(:impressions_count)
+    @impression= Impression.where(created_at: start_date..end_date).group('DAY(created_at)').count
     
+
   end
 
   def show
