88bba9303358dbbc7118b8ce273743215b7c677b
mimisenn <53231640+mimisenn@users.noreply.github.com>
2020-04-20 22:30:33 +0900

    ポイント取得更新

--- app/controllers/impressions_controller.rb
+++ app/controllers/impressions_controller.rb
@@ -1,10 +1,10 @@
 class ImpressionsController < ApplicationController
   def day_count
+    @date = Date.current.strftime('%Y/%m')
     start_date = Time.current.beginning_of_month
     end_date = Time.current.end_of_month
     @impression = Impression.where(created_at: start_date..end_date).group("DAY(created_at)").count
     @impressions = Impression.group("MONTH(created_at)").count
-    @date = Date.current.strftime('%Y/%m')
 
   end
   def month_count
--- app/controllers/user_points_controller.rb
+++ app/controllers/user_points_controller.rb
@@ -1,17 +1,14 @@
 class UserPointsController < ApplicationController
 
   def day_point
-    range = Time.current.beginning_of_day..Time.current.end_of_day
-    # def amount
-    #   user_points.sum(:amount)
-    # end
-    # @amout = user_points.amount
-    # @user.user_points
-    # if current_user.where(current_sign_in_at: range) != @user.where(last_sign_in_at: range) 
-    #   amount += 1
-    # else
-    #   nil
-    # end
+    @date = Date.current.strftime('%Y/%m')
+    @users = User.group("DAY(created_at)").count
+    @impressions = Impression.group("MONTH(created_at)").count
   end
 
+  def month_point
+    @date = Date.current.strftime('%Y')
+    @users = User.group("MONTH(created_at)").count
+    @impressions = Impression.group("MONTH(created_at)").count
+  end
 end
