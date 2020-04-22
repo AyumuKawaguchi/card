c0fa09d3d81952561a223a83b22024c8ebcf4649
mimisenn <53231640+mimisenn@users.noreply.github.com>
2020-04-22 14:37:00 +0900

    Update impressions_controller.rb

--- app/controllers/impressions_controller.rb
+++ app/controllers/impressions_controller.rb
@@ -3,7 +3,7 @@ class ImpressionsController < ApplicationController
     @date = Date.current.strftime('%Y/%m')
     start_date = Time.current.beginning_of_year
     end_date = Time.current.end_of_year
-    @impression = Impression.where(created_at: start_date..end_date).group("date(created_at)").count
+    @impression = Impression.group("DAY(created_at)").count
     @impressions = Impression.group("MONTH(created_at)").count
     @chart = Impression.group("date(created_at)").count
   end
