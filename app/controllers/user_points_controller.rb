9e13333aae181ae2a7127f4746fd0495d3d36eda
mimisenn <53231640+mimisenn@users.noreply.github.com>
2020-04-22 17:16:01 +0900

    微修正

--- app/controllers/user_points_controller.rb
+++ app/controllers/user_points_controller.rb
@@ -2,12 +2,12 @@ class UserPointsController < ApplicationController
 
   def day_point
     @date = Date.current.strftime('%Y/%m')
-    @users = User.group("DAY(created_at)").count
+    @users = User.group("DATE(created_at)").count
     @impressions = Impression.group("MONTH(created_at)").count
   end
 
   def month_point
-    @date = Date.current.strftime('%Y')
+    @date = Date.today.strftime('%Y')
     @users = User.group("MONTH(created_at)").count
     @impressions = Impression.group("MONTH(created_at)").count
   end
