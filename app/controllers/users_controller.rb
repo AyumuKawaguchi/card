a5907aba6292fcfc335eace3c34f38ce61bb7cd2
mimisenn <53231640+mimisenn@users.noreply.github.com>
2020-04-29 08:23:55 +0900

    検索条件追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,13 +12,19 @@ class UsersController < ApplicationController
   end
 
   def search
+
+    start_date = Time.current.beginning_of_day
+    end_date = Time.current.end_of_day
+    @user = User.all.sum(:impressions_count)
+    @impression= Impression.where(created_at: start_date..end_date).group('DAY(created_at)').count
+    @impressions = Impression.group("MONTH(created_at)").count
     if params[:q].present?
       @q = User.ransack(search_params)
       @users = @q.result
-    # else
-    #   params[:q] = {sorts: 'id_desc'}
-    #   @q = User.ransack()
-    #   @users = User.all
+    else
+      params[:q] = {sorts: 'id_desc'}
+      @q = User.ransack()
+      @users = User.all
     end
 
   end
