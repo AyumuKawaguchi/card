b07ed09e321d2335eb13c5410b9a4b557450f5d1
mimisenn <53231640+mimisenn@users.noreply.github.com>
2020-04-30 16:40:25 +0900

    編集・削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,9 +40,31 @@ class UsersController < ApplicationController
     end
   end
  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
+  end
+
   def search_params
     params.require(:q).permit(:sorts)
   end
+  private
 
-
+  def user_params
+    params.require(:user).permit(:username, :email, :points)
+  end
 end
